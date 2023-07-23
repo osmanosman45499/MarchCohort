7b076831e019e0222b009575de91867a295cb13a
Double O <doubleo@Doubles-MacBook-Air.local>
2023-07-23 14:29:34 +0100

    updating homework for lesson1

---
+++ javascript/homework/homework1.js
@@ -0,0 +1,19 @@
+//q1 using string concatentation add two different strings together and print this off 
+
+console.log("Hello" + " World")
+
+//q2 Using variables create a variable for a number, a string. Then print each of these variables.
+
+var firstword = "Hello"
+var secondword = "2"
+
+console.log(firstword)
+console.log(secondword)
+
+//q3 Using string concatenation in javascript combine 3 different variables and prints it off. I.e variable for name age and date of birth will result in “my name is abdulkadir I am 24 years old and I was born in 1998”. (you cannot use name age and dob as your variables).
+
+var name = "Osman"
+var age = "22" 
+var dob = "2001"
+
+console.log("my name is" , name, "I am years", age , "old and i was born in" , dob)
