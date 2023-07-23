b4acef7404a1fb39dd51a19c57d6a473df593636
Double O <doubleo@Doubles-MacBook-Air.local>
2023-07-23 14:32:50 +0100

    upadting lesson 1

--- javascript/lessons/lesson1.js
+++ javascript/lessons/lesson1.js
@@ -1,3 +1,39 @@
 //this is a single line comment
 
 console.log("Hello you have executed the lesson 1 file")
+
+console.log("hello world!");
+
+var name = "Osman"
+
+console.log(name);
+
+var myName = "osman";
+
+var myAgeIs ="22";
+
+console.log(myName);
+
+console.log(myAgeIs);
+
+var greeting = "Hey there"
+
+var myName= " Osman"
+
+console.log(greeting+myName)
+
+console.log(3434+343443)
+
+console.log("Hello" +" world!")
+
+var firstword = "Hello"
+
+var secondword = "world"
+
+console.log(firstword + " " + secondword) 
+
+var firstword = "Hello"
+var secondword= "2"
+
+console.log(firstword)
+console.log(secondword)
